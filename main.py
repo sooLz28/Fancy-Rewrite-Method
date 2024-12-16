01c200e49ca31b93ba1282cd1e9b4023fb1bfe54
aboodkh12 <aboodmarwan877@gmail.com>
2024-12-16 22:23:08 +0300

    Update main.py
    added numbers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 input = input("Enter a Sentence: ")
-allLetters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBN!@#$%^&*()_+-= "
+allLetters = "qwe8rtyuiopa1s9dfgh5jklzxcvbnm2QWE4RTYU0IOPA7S3DFG6HJKLZXCVBN!@#$%^&*()_+-= "
 output = ""
 i = 0
 while output != input:
